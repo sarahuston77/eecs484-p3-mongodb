9cbd79cd7a895ab441ed6baad494289eb0d99fb3
Sara Huston <shuston007@gmail.com>
2025-10-27 21:19:25 -0400

    fixed q1

--- query1.js
+++ query1.js
@@ -7,10 +7,10 @@ function find_user(city, dbname) {
 
     let results = [];
     // TODO: find all users who live in city
-    results = db.users.find({"current.city" : city }, {user_id : 1}).map( 
+    results = db.users.find({"hometown.city" : city }, {user_id : 1}).map( 
         (p) => { return p.user_id }).toArray();
     
     // See test.js for a partial correctness check.
-	
+	print(results);
     return results;
 }
